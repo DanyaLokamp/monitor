ac2d66ff177de68911ec9c574847edb5ce6771b9
daniilselivanov <DanyaLokamp@gmail.com>
2020-08-17 02:37:27 +0300

    * .gitignore: Initial structure
    * SharedResources.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * MainLogic.cs:
    * SharedResources.csproj:
    * appsettings.Development.json:
    * launchSettings.json:

---
+++ SharedResources/Logic/MainLogic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using SharedResources.Models;
+
+namespace SharedResources.Logic
+{
+    public class MainLogic : IMainLogic
+    {
+        private readonly ISharedResourceLogic _sharedResourceLogic;
+
+        public MainLogic(
+            ISharedResourceLogic sharedResourceLogic)
+        {
+            _sharedResourceLogic = sharedResourceLogic;
+        }
+
+        public async Task<(bool isException, Guid, string)> LockResource(Info user)
+        {
+            try
+            {
+                var res = await _sharedResourceLogic.TryEnterAsync(user.SomeData);
+
+                if (res.Item1)
+                    return (false, res.Item2, "Successfully locked!");
+
+                return (true, Guid.Empty, "Already locked!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //return (true, Guid.Empty, ex.Message);
+            }
+        }
+
+        public (bool success, bool isException, string message) ReleaseResource(SynchronizationSettings settings)
+        {
+            try
+            {
+                if (_sharedResourceLogic.TryExit(settings.SyncKey))
+                    return (true, false, "Successfully unlocked!");
+
+                return (false, false, "Nothing to unlock!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //return (false, true, ex.Message);
+            }
+        }
+    }
+}
